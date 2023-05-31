0e3d608bbac92a4405e065e436d13959889392b9
BilalStudy <biah001@student.kristiania.no>
2023-05-31 18:45:40 +0200

    second commit with server

---
+++ server/api/userApi.js
@@ -0,0 +1,111 @@
+import express, {request} from "express";
+import {ObjectId} from "mongodb";
+
+export function UserApi(db){
+    const api = express.Router();
+
+    api.post('/register', async (req, res) => {
+            const {username, fullname, password, role} = req.body
+
+            if (!username || username.length == 0) {
+                res.sendStatus(500);
+                return;
+            }
+
+
+            if (!fullname || fullname.length == 0) {
+                res.sendStatus(500);
+                return;
+            }
+
+            if (!password || password.length == 0) {
+                res.sendStatus(500);
+                return;
+            }
+
+            if (!role || role.length == 0) {
+                res.sendStatus(500);
+                return;
+            }
+
+            const users = await db.collection("users")
+                .find({ username: username })
+                .toArray();
+
+            if (users.length > 0) {
+                res.sendStatus(500);
+                return;
+            }
+
+            const result = db.collection("users").insertOne({
+                username,
+                fullname,
+                password,
+                role,
+            });
+
+            res.sendStatus(200);
+        }
+        ,
+        api.post('/login', async (req, res) => {
+            const {username, password} = req.body;
+
+            const users = await db.collection("users")
+                .find({username: username, password: password})
+                .toArray();
+
+            if (users.length > 0){
+                const user = users[0];
+
+                res.json({
+                    cookie : user._id.toString(),
+                    user: user,
+                })
+            }else {
+                res.sendStatus(404);
+            }
+        })
+        ,
+        api.get('/logged-in', async (req,res) => {
+            if (!req.cookies.user || req.cookies.user.length == 0) {
+                res.sendStatus(403);
+                return;
+            }
+
+            const user = await db.collection("users")
+                .findOne({ _id: new ObjectId(req.cookies.user) });
+
+            if (!user) {
+                res.sendStatus(404);
+                return;
+            }
+
+            res.send(user);
+        }),
+        api.get("/", async (req, res) => {
+            /*
+            const managerId = req.cookies.manager;
+
+            if (!managerId) {
+                res.sendStatus(403);
+                return;
+            }
+            */
+
+            const listAllUsers = await db.collection("users")
+                .find()
+                .map(({ _id,username, fullname, password, role }) => ({
+                    id: _id,
+                    username,
+                    fullname,
+                    password,
+                    role,
+                }))
+                .toArray();
+
+            res.json(listAllUsers)
+        })
+    )
+
+    return api;
+}
---
+++ server/server.js
@@ -0,0 +1,66 @@
+import {MongoClient, ObjectId} from "mongodb";
+import dotenv from "dotenv";
+import express from "express";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import path from "path";
+import { UserApi } from "./api/userApi.js"
+import {WebSocketServer} from "ws";
+
+dotenv.config();
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+
+const mongodburl = process.env.MONGODB_URL;
+
+if(mongodburl){
+    const client = new MongoClient(mongodburl);
+
+    const mongoDbName = process.env.MONGODB_DATABASE || "smidigdb";
+
+    client.connect().then(async (conn) => {
+        app.use("/api/users", UserApi(conn.db(mongoDbName)));
+    })
+}
+
+
+const sockets = [];
+
+const wsServer = new WebSocketServer({noServer: true})
+
+wsServer.on("connection", (socket) => {
+    sockets.push(socket);
+    socket.on("message", (message) => {
+        console.log("There's a message" + message)
+        for (const reciepient of sockets) {
+            reciepient.send(message.toString());
+        }
+    })
+});
+
+/*
+app.use((req, res, next) => {
+    if (req.method === "GET" && !req.path.startsWith("/api")) {
+        return res.sendFile(path.resolve("../client/dist/index.html"));
+    } else {
+        next();
+    }
+});
+*/
+
+const server = app.listen(process.env.PORT || 3000,
+    () => {
+        console.log(`express server started on: http://localhost:${server.address().port}`)
+
+        server.on("upgrade", (req, socket, head) => {
+            wsServer.handleUpgrade(req, socket, head, (socket) => {
+                console.log("connected");
+                wsServer.emit("connection", socket, req)
+            })
+        })
+    });
