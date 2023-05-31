5530c73af1f59a76a7b95b4c002fa54088111734
BilalStudy <biah001@student.kristiania.no>
2023-06-01 00:52:17 +0200

    commiting api and home and LoginScreen

---
+++ AppChew/api/userApi.jsx
@@ -0,0 +1,34 @@
+export const userApi = {
+
+    getLoggedInUser: async () => {
+        const res = await fetch("http://localhost:3000/api/users/logged-in");
+
+        return res.ok ? await res.json() : null;
+    },
+    registerNewUser:async (user) => {
+        const res = await fetch('http://localhost:3000/api/users/register', {
+            method : "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(user),
+        })
+
+        return res.ok;
+    },
+
+    userLogin : async (username, password) => {
+        const res = await fetch("http://localhost:3000/api/users/login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                username,
+                password,
+            }),
+        })
+
+        return res.ok ? await res.json() : null
+    },
+}
