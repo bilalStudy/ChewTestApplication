98443b78c8bb8fe3f890b0def17db21effb5e04d
eld-ray <joneldraytamon@gmail.com>
2023-06-01 22:22:09 +0200

    Src folders, Interface, and Login endpoint in Service

--- AppChew/App.jsx
+++ AppChew/App.jsx
@@ -9,17 +9,18 @@ const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen options={{headerShown: false}} name="Login" component={LoginScreen} />
-          <Stack.Screen name="Home" component={HomeScreen} />
-        </Stack.Navigator>
-      </NavigationContainer>
-
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          options={{ headerShown: false }}
+          name="Login"
+          component={LoginScreen}
+        />
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-
 
 const styles = StyleSheet.create({
   container: {
--- AppChew/src/services/userApi.ts
+++ AppChew/src/services/userApi.ts
@@ -1,5 +1,12 @@
+import axios from 'axios';
+
 const UserApi = (() => {
   const userApiEndpoints = {
-    user: 'http://localhost:3000/api/users',
+    login: 'http://localhost:3000/api/users/login',
+  };
+
+  const getTeacherUser = async (username: string, password: string) => {
+    const result = await axios.get(userApiEndpoints.login);
+    return result;
   };
 })();
