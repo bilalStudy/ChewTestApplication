0ff0317709afcae453271d01bd515a5b0723d651
BilalStudy <biah001@student.kristiania.no>
2023-06-02 12:49:20 +0200

    changes made to network in frontend

--- AppChew/api/userApi.jsx
+++ AppChew/api/userApi.jsx
@@ -1,7 +1,15 @@
 import { Platform } from 'react-native'
+import {NetworkInfo} from "react-native-network-info";
+
+NetworkInfo.getIPV4Address().then(ipv4 => {
+    const ipv4Address = ipv4;
+   console.log(ipv4);
+})
 
 export const baseUrl = Platform.OS === 'android' ?
     'http://10.0.2.2:3000' : 'http://localhost:3000';
+
+
 
 export const userApi = {
 
