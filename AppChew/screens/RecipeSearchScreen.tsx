8f5849235191458b6e0e6f831b32b0288f0b5a87
eld-ray <joneldraytamon@gmail.com>
2023-06-06 13:34:47 +0200

    Code structure

--- AppChew/screens/RecipeSearchScreen.tsx
+++ AppChew/screens/RecipeSearchScreen.tsx
@@ -1,43 +1,43 @@
-import {Image, StyleSheet, Text, View} from 'react-native'
-import React, {useEffect, useState} from 'react'
-import MainContainer from "../App";
-import {recipeApi} from "../api/recipeApi";
-
-
+import { Image, StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import MainContainer from '../App';
+import { recipeApi } from '../api/recipeApi';
 
 const RecipeSearchScreen = () => {
-    const [recipes, setRecipes] = useState([])
+  const [recipes, setRecipes] = useState([]);
 
-    useEffect(() => {
-        (async () => {
-            setRecipes(await recipeApi.listAll());
-        })();
-    }, []);
+  useEffect(() => {
+    (async () => {
+      setRecipes(await recipeApi.listAll());
+    })();
+  }, []);
 
-    // gi styleName for individuell style
+  // gi styleName for individuell style
 
-    return (
-        <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-            <Text>Recipe Screen</Text>
-            <View>
-                {recipes.map((x) =>(
-                    <View key={x.id}>
-                        <Image style={{
-                            resizeMode: 'contain',
-                            width: '50%',
-                            height: '50%',
-                        }} source={{
-                            uri: `${x.picture}`,
-                        }}></Image>
-                        <Text>dishname : {x.dishname}</Text>
-                    </View>
-                )) }
-            </View>
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Recipe Screen</Text>
+      <View>
+        {recipes.map((x) => (
+          <View key={x.id}>
+            <Image
+              style={{
+                resizeMode: 'contain',
+                width: '50%',
+                height: '50%',
+              }}
+              source={{
+                uri: `${x.picture}`,
+              }}
+            ></Image>
+            <Text>dishname : {x.dishname}</Text>
+          </View>
+        ))}
+      </View>
+    </View>
+  );
+};
 
-        </View>
-    )
-}
+export default RecipeSearchScreen;
 
-export default RecipeSearchScreen
-
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
