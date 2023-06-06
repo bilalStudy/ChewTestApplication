389d204765df4417747d644cb755226d6299844d
John Velo <johnnvelo7@gmail.com>
2023-06-06 21:38:58 +0200

    Commit for 06/06/23

--- server/api/recipeApi.js
+++ server/api/recipeApi.js
@@ -4,9 +4,7 @@ import {ObjectId} from "mongodb";
 export function RecipeApi(db){
     const api = express.Router();
 
-        api.get("/", async (req, res) => {
-
-
+        api.get("/", async (req, res) => {try {
             const listAllRecipe = await db.collection("recipe")
                 .find()
                 .map(({ _id, dishname, picture, guide, description, nutrition, allergens, ingredients, kitchentools, category, culture }) => ({
@@ -23,9 +21,13 @@ export function RecipeApi(db){
                     culture
                 }))
                 .toArray();
-
-            res.json(listAllRecipe)
-        })
+    
+            res.json(listAllRecipe);
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ message: 'An error occurred while getting the recipes.' });
+        }
+    })
 
     return api;
 }
