f780e34a2608e94133a7bb9d9241f8c0a77361bc
matsjohaa <mmaatts@gmail.com>
2023-06-06 11:12:36 +0200

    Edited navigation

--- AppChew/MainContainer.jsx
+++ AppChew/MainContainer.jsx
@@ -1,5 +1,5 @@
 import HomeScreen from "./screens/HomeScreen";
-import SettingsScreen from "./screens/SettingsScreen";
+import Profile from "./screens/Profile";
 import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
 import Ionicons from "react-native-vector-icons/Ionicons";
 import QuizScreen from "./screens/QuizScreen";
@@ -9,7 +9,7 @@ import RecipeSearchScreen from "./screens/RecipeSearchScreen";
 const homeName= 'Announcements';
 const quizName = 'Quizes';
 const recipiesName = 'Recipies';
-const settingName = 'Settings';
+const profileName = 'Profile';
 
 const Tab = createBottomTabNavigator()
 
@@ -32,8 +32,8 @@ export default function MainContainer() {
 
                         } else if (rn === recipiesName) {
                             iconName = focused ? 'restaurant' : 'restaurant-outline';
-                        } else if (rn === settingName) {
-                            iconName = focused ? 'settings' : 'settings-outline';
+                        } else if (rn === profileName) {
+                            iconName = focused ? 'happy' : 'happy-outline';
                         }
 
 
@@ -50,7 +50,7 @@ export default function MainContainer() {
                 <Tab.Screen name={homeName} component={HomeScreen}/>
                 <Tab.Screen name={quizName} component={QuizScreen}/>
                 <Tab.Screen name={recipiesName} component={RecipeSearchScreen}/>
-                <Tab.Screen name={settingName} component={SettingsScreen}/>
+                <Tab.Screen name={profileName} component={Profile}/>
 
             </Tab.Navigator>
 
