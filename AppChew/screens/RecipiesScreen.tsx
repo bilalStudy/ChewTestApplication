749dc66db0003f5d59f6d540b9b69bf71b8b1434
varunthileepan <varunthileepan@gmail.com>
2023-06-06 11:18:27 +0200

    Recipe screen mod

--- AppChew/screens/RecipiesScreen.tsx
+++ AppChew/screens/RecipiesScreen.tsx
@@ -1,16 +1,74 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {Image, StyleSheet, Text, View} from 'react-native'
+import React, {useEffect, useState} from 'react'
 import MainContainer from "../App";
+import {recipeApi} from "../api/recipeApi";
 
 
-const RecipeScreen = () => {
-    return (
-        <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-            <Text>Recipe Screen</Text>
 
-        </View>
-    )
+interface Recipe {
+    id: string;
+    dishname: string;
+    picture: string;
+    guide: string;
+    description: string;
+    nutrition: string;
+    allergens: string;
+    ingredients: string[];
+    kitchentools: string[];
+    category: string;
+    culture: string;
 }
+
+
+const RecipeScreen: React.FC = () => {
+    const [recipes, setRecipes] = useState<Recipe[]>([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const fetchedRecipes = await recipeApi.listAll();
+                setRecipes(fetchedRecipes);
+            } catch (error) {
+                console.error('Error fetching recipes:', error);
+            }
+        };
+
+        fetchData().catch(error => {
+            console.error('Error fetching recipes:', error);
+        });
+    }, []);
+
+
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text>Recipe Screen</Text>
+            <View>
+                {recipes.map(recipe => (
+                    <View key={recipe.id}>
+                        <Image
+                            style={{
+                                resizeMode: 'contain',
+                                width: '50%',
+                                height: '50%',
+                            }}
+                            source={{
+                                uri: recipe.picture,
+                            }}
+                        />
+                        <Text>Dish Name: {recipe.dishname}</Text>
+                        <Text>Guide: {recipe.guide}</Text>
+                        <Text>Description: {recipe.description}</Text>
+                        <Text>Nutrition: {recipe.nutrition}</Text>
+                        <Text>Allergens: {recipe.allergens}</Text>
+                        <Text>Category: {recipe.category}</Text>
+                        <Text>Culture: {recipe.culture}</Text>
+                    </View>
+                ))}
+            </View>
+        </View>
+    );
+};
+
 
 export default RecipeScreen
 
