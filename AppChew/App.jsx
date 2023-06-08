2f0f19a5454f77717722e34d77a4946253ddd5f5
matsjohaa <mmaatts@gmail.com>
2023-06-08 13:00:16 +0200

    worked on login

--- AppChew/App.jsx
+++ AppChew/App.jsx
@@ -21,7 +21,7 @@ export default function App() {
       <NavigationContainer>
         <Stack.Navigator>
           <Stack.Screen options={{headerShown: false}} name="Login" component={LoginScreen} />
-          <Stack.Screen name="Home" component={MainContainer} />
+          <Stack.Screen options={{headerShown: false}} name="Home" component={MainContainer} />
         </Stack.Navigator>
       </NavigationContainer>
       </AuthProvider>
