0176a07799415c417ce7b0150a6dcef4fea45f6b
varunthileepan <varunthileepan@gmail.com>
2023-06-14 10:49:10 +0200

    Added score tracker to quiz

--- AppChew/screens/QuizScreen.tsx
+++ AppChew/screens/QuizScreen.tsx
@@ -25,6 +25,8 @@ interface QuizItem {
 
 const QuizScreen = () => {
   const [quizzes, setQuizzes] = useState<QuizItem[]>([]);
+  const [score, setScore] = useState(0);
+  const [totalPoints, setTotalPoints] = useState(0);
 
   useEffect(() => {
     fetchQuizzes();
@@ -34,31 +36,60 @@ const QuizScreen = () => {
     try {
       const fetchedQuizzes = await listQuizzes();
       setQuizzes(fetchedQuizzes);
+      calculateTotalPoints(fetchedQuizzes);
     } catch (error) {
       console.error(error);
     }
+  };
+
+  const calculateTotalPoints = (quizzes: QuizItem[]) => {
+    let points = 0;
+    quizzes.forEach((quiz) => {
+      points += quiz.questions.length;
+    });
+    setTotalPoints(points);
   };
 
   const handleAnswer = (questionId: string, isTrue: boolean) => {
     setQuizzes((prevQuizzes) => {
       const updatedQuizzes = prevQuizzes.map((quiz) => {
         if (quiz.questions.some((q) => q.question === questionId)) {
+          let questionAnswered = false; // Flag to check if the question has already been answered
+
+          const updatedQuestions = quiz.questions.map((question) => {
+            if (question.question === questionId) {
+              if (question.userAnswer !== undefined) {
+                // If the question has already been answered, don't update anything
+                questionAnswered = true;
+                return question;
+              }
+
+              const answerStatus = isTrue === question.answer;
+              const scoreIncrement = answerStatus ? 1 : 0;
+              setScore((prevScore) => prevScore + scoreIncrement);
+
+              return {
+                ...question,
+                userAnswer: isTrue,
+                answerStatus: answerStatus,
+              };
+            }
+            return question;
+          });
+
+          // If the question has already been answered, don't update the quiz
+          if (questionAnswered) {
+            return quiz;
+          }
+
           return {
             ...quiz,
-            questions: quiz.questions.map((question) => {
-              if (question.question === questionId) {
-                return {
-                  ...question,
-                  userAnswer: isTrue,
-                  answerStatus: isTrue === question.answer,
-                };
-              }
-              return question;
-            }),
+            questions: updatedQuestions,
           };
         }
         return quiz;
       });
+
       return updatedQuizzes;
     });
   };
@@ -66,134 +97,150 @@ const QuizScreen = () => {
   const screenWidth = Dimensions.get('window').width;
 
   return (
-    <View style={styles.container}>
-      <View
-        style={{
-          width: '80%',
-          padding: 10,
-          borderRadius: 10,
-          alignItems: 'center',
-        }}
-      >
-        <Image
-          source={require('../assets/background2.jpg')}
-          style={{
-            resizeMode: 'cover',
-            width: '100%',
-            height: 150,
-            borderRadius: 10,
-          }}
+      <View style={styles.container}>
+        <View
+            style={{
+              width: '80%',
+              padding: 10,
+              borderRadius: 10,
+              alignItems: 'center',
+            }}
+        >
+          <Image
+              source={require('../assets/background2.jpg')}
+              style={{
+                resizeMode: 'cover',
+                width: '100%',
+                height: 150,
+                borderRadius: 10,
+              }}
+          />
+        </View>
+        <Text style={{ left: '25%' }}>(13k Reviews)</Text>
+
+        <FlatList<QuizItem>
+            data={quizzes}
+            keyExtractor={(item) => item.id}
+            renderItem={({ item }) => (
+                <View style={styles.quizItem}>
+                  <Text style={{ left: '5%' }}>Quiz Name: {item.quizName}</Text>
+                  {item.questions.map((questionObj, index) => (
+                      <View
+                          key={index}
+                          style={[styles.quizItem, { width: screenWidth * 0.8 }]}
+                      >
+                        <View style={styles.questionBox}>
+                          <Text>{questionObj.question}</Text>
+                        </View>
+                        <View style={styles.buttonContainer}>
+                          <View
+                              style={[
+                                styles.button,
+                                {
+                                  backgroundColor:
+                                      questionObj.userAnswer !== undefined
+                                          ? questionObj.userAnswer === true
+                                              ? questionObj.answerStatus
+                                                  ? '#34A853'
+                                                  : '#EA4335'
+                                              : 'transparent'
+                                          : 'transparent',
+                                },
+                              ]}
+                          >
+                            <Button
+                                title="True?"
+                                onPress={() => handleAnswer(questionObj.question, true)}
+                                color={
+                                  questionObj.userAnswer !== undefined &&
+                                  questionObj.userAnswer === true
+                                      ? '#fff'
+                                      : '#F86D47'
+                                }
+                            />
+                          </View>
+
+                          <View
+                              style={[
+                                styles.button,
+                                {
+                                  backgroundColor:
+                                      questionObj.userAnswer !== undefined
+                                          ? questionObj.userAnswer === false
+                                              ? questionObj.answerStatus
+                                                  ? '#34A853'
+                                                  : '#EA4335'
+                                              : 'transparent'
+                                          : 'transparent',
+                                },
+                              ]}
+                          >
+                            <Button
+                                title="False?"
+                                onPress={() => handleAnswer(questionObj.question, false)}
+                                color={
+                                  questionObj.userAnswer !== undefined &&
+                                  questionObj.userAnswer === false
+                                      ? '#fff'
+                                      : '#F86D47'
+                                }
+                            />
+                          </View>
+                        </View>
+                      </View>
+                  ))}
+                </View>
+            )}
         />
+
+        <View style={styles.scoreContainer}>
+          <Text style={styles.scoreText}>
+            Score: {score}/{totalPoints}
+          </Text>
+        </View>
       </View>
-      <Text style={{ left: '25%' }}>(13k Reviews)</Text>
-
-      <FlatList<QuizItem>
-        data={quizzes}
-        keyExtractor={(item) => item.id}
-        renderItem={({ item }) => (
-          <View style={styles.quizItem}>
-            <Text style={{ left: '5%' }}>Quiz Name: {item.quizName}</Text>
-            {item.questions.map((questionObj, index) => (
-              <View
-                key={index}
-                style={[styles.quizItem, { width: screenWidth * 0.8 }]}
-              >
-                <View style={styles.questionBox}>
-                  <Text>{questionObj.question}</Text>
-                </View>
-                <View style={styles.buttonContainer}>
-                  <View
-                    style={[
-                      styles.button,
-                      {
-                        backgroundColor:
-                          questionObj.userAnswer !== undefined
-                            ? questionObj.userAnswer === true
-                              ? questionObj.answerStatus
-                                ? '#34A853'
-                                : '#EA4335'
-                              : 'transparent'
-                            : 'transparent',
-                      },
-                    ]}
-                  >
-                    <Button
-                      title="True?"
-                      onPress={() => handleAnswer(questionObj.question, true)}
-                      color={
-                        questionObj.userAnswer !== undefined &&
-                        questionObj.userAnswer === true
-                          ? '#fff'
-                          : '#F86D47'
-                      }
-                    />
-                  </View>
-
-                  <View
-                    style={[
-                      styles.button,
-                      {
-                        backgroundColor:
-                          questionObj.userAnswer !== undefined
-                            ? questionObj.userAnswer === false
-                              ? questionObj.answerStatus
-                                ? '#34A853'
-                                : '#EA4335'
-                              : 'transparent'
-                            : 'transparent',
-                      },
-                    ]}
-                  >
-                    <Button
-                      title="False?"
-                      onPress={() => handleAnswer(questionObj.question, false)}
-                      color={
-                        questionObj.userAnswer !== undefined &&
-                        questionObj.userAnswer === false
-                          ? '#fff'
-                          : '#F86D47'
-                      }
-                    />
-                  </View>
-                </View>
-              </View>
-            ))}
-          </View>
-        )}
-      />
-    </View>
   );
 };
-
-export default QuizScreen;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-
     alignItems: 'center',
     justifyContent: 'center',
   },
   quizItem: {
-    margin: 10,
-    padding: 10,
-    backgroundColor: '#f5f5f5',
-    borderRadius: 10,
+    marginTop: 10,
+    marginBottom: 10,
+    alignItems: 'center',
   },
   questionBox: {
-    height: 150,
-    marginBottom: '5%',
+    width: '100%',
     padding: 10,
-    borderColor: '#000',
+    backgroundColor: '#f0f0f0',
     borderRadius: 10,
-    backgroundColor: '#D9D9D9',
+    marginBottom: 10,
+    borderWidth: 1,
+    borderColor: '#ccc',
   },
+
   buttonContainer: {
     flexDirection: 'row',
-    justifyContent: 'space-between',
+    justifyContent: 'center',
   },
   button: {
-    width: '50%',
-    borderRadius: 10,
+    flex: 1,
+    margin: 5,
+    borderRadius: 5,
+  },
+  scoreContainer: {
+    marginTop: 20,
+    alignItems: 'center',
+  },
+  scoreText: {
+    fontSize: 18,
+    fontWeight: 'bold',
   },
 });
+
+export default QuizScreen;
+
